18c7df7caadf1c845a83b0daf8ec0e1416d39065
DmDespert <ordimac@jim-dede.(none)>
2021-04-08 09:41:02 +0200

    fix: date format

--- today.js
+++ today.js
@@ -1,1 +1,4 @@
-console.log(new Date);
+const months = ["JAN", "FEB", "MAR","APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+let current_datetime = new Date()
+let formatted_date = current_datetime.getDate() + "-" + months[current_datetime.getMonth()] + "-" + current_datetime.getFullYear()
+console.log(formatted_date)
